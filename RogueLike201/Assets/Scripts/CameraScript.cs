27488a9942ae14c25db1a4185df413620fcd4334
16appletond <dappleto@usc.edu>
2020-03-19 01:08:19 -0500

    Added the project folder

---
+++ RogueLike201/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Rigidbody2D playerRB;
+    public GameObject Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = Player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(playerRB.position.x, playerRB.position.y + 3.33f, -10.0f);
+    }
+}
