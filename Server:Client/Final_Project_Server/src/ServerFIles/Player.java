a60c247d71034e459d9b57ee71324d4e6ec63d53
ChrisConcho <concho@usc.edu>
2020-04-11 19:07:38 -0700

    Server First upload

---
+++ Server:Client/Final_Project_Server/src/ServerFIles/Player.java
@@ -0,0 +1,98 @@
+package ServerFIles;
+
+import java.util.List;
+import java.util.Vector;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Player {
+
+@SerializedName("PlayerID")
+@Expose
+private Integer playerID;
+@SerializedName("NAME")
+@Expose
+private String nAME;
+@SerializedName("HighScore")
+@Expose
+private Integer highScore;
+@SerializedName("AllHighScoreVect")
+@Expose
+private List<Integer> AllhighScore;
+@SerializedName("NewGamePlus")
+@Expose
+private Boolean newGamePlus;
+@SerializedName("Cosmetic")
+@Expose
+private List<Boolean> cosmetic = null;
+@SerializedName("WeaponID")
+@Expose
+private Integer weaponID;
+@SerializedName("Money")
+@Expose
+private Integer money;
+
+public Integer getPlayerID() {
+return playerID;
+}
+
+public void setPlayerID(Integer playerID) {
+this.playerID = playerID;
+}
+
+public String getNAME() {
+return nAME;
+}
+
+public void setNAME(String nAME) {
+this.nAME = nAME;
+}
+
+public Integer getHighScore() {
+return highScore;
+}
+
+public void setHighScore(Integer highScore) {
+this.highScore = highScore;
+}
+public List<Integer> getAllHighScore(){
+	return AllhighScore;
+}
+public void setAllHighScore(List<Integer> AllHighScore) {
+	this.AllhighScore= AllHighScore;
+}
+
+public Boolean getNewGamePlus() {
+return newGamePlus;
+}
+
+public void setNewGamePlus(Boolean newGamePlus) {
+this.newGamePlus = newGamePlus;
+}
+
+public List<Boolean> getCosmetic() {
+return cosmetic;
+}
+
+public void setCosmetic(List<Boolean> cosmetic) {
+this.cosmetic = cosmetic;
+}
+
+public Integer getWeaponID() {
+return weaponID;
+}
+
+public void setWeaponID(Integer weaponID) {
+this.weaponID = weaponID;
+}
+
+public Integer getMoney() {
+return money;
+}
+
+public void setMoney(Integer money) {
+this.money = money;
+}
+
+}
---
+++ Server:Client/Final_Project_Server/src/ServerFIles/Server.java
@@ -0,0 +1,44 @@
+package ServerFIles;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+	public ServerSocket serverSocket;
+
+	public Server(int port) {
+		// to do --> implement your constructor
+			
+			try {
+				serverSocket = new ServerSocket(port);
+			
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Server server= new Server(9999);
+		while(true) {
+			Socket socket;
+			try {
+				socket = server.serverSocket.accept();
+			
+				ServerThread serverThread = new ServerThread(socket);
+				System.out.println("Given a Thread");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}
+	}
+
+}
