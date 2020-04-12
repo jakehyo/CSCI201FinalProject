7a04239bf811aef2330e37af20f9c10b81c884d1
Jeffrey Han <jskh1999@gmail.com>
2020-04-11 21:55:53 -0700

    trying to fix my fuck up

--- RogueLike201/Assets/Scripts/PlayerScript.cs
+++ RogueLike201/Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     public Rigidbody2D rb;
     public int moveVal;
     public GameObject projectile;
+    public GameObject crossHair;
     public Transform projectileSpawn;
     private float timeBtwShots;
     public float startTimeBtwShots;
@@ -19,31 +20,50 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 mouseDirection = new Vector2(mousePosition.x,mousePosition.y);
+        //Debug.Log("mouseDirection: " + mouseDirection + ", player:" + rb.position);
+        crossHair.transform.position = mouseDirection;
+
+        Vector2 playertoCursor = new Vector2(mousePosition.x - rb.position.x, mousePosition.y - rb.position.y);
+        float angle = Vector2.SignedAngle(new Vector2(0.0f,1.0f), playertoCursor);
+        rb.rotation = angle;
+
+
+        Debug.Log(angle);
+
         if (Input.GetKey(KeyCode.W))
         {
-            rb.rotation = 0.0f;
+            //rb.rotation = 0.0f;
             rb.position = new Vector2(rb.position.x, rb.position.y + (moveVal * Time.deltaTime));
+            //crossHair.transform.position = new Vector2(crossHair.transform.position.x, crossHair.transform.position.y +(moveVal*Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.rotation = 90.0f;
+            //rb.rotation = 90.0f;
             rb.position = new Vector2(rb.position.x - (moveVal * Time.deltaTime), rb.position.y );
+            //crossHair.transform.position = new Vector2(crossHair.transform.position.x - (moveVal * Time.deltaTime), crossHair.transform.position.y);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.rotation = 180.0f;
+            //rb.rotation = 180.0f;
             rb.position = new Vector2(rb.position.x, rb.position.y - (moveVal * Time.deltaTime));
+            //crossHair.transform.position = new Vector2(crossHair.transform.position.x, crossHair.transform.position.y - (moveVal * Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.rotation = 270.0f;
+            //rb.rotation = 270.0f;
             rb.position = new Vector2(rb.position.x + (moveVal * Time.deltaTime), rb.position.y);
+            //crossHair.transform.position = new Vector2(crossHair.transform.position.x + (moveVal * Time.deltaTime), crossHair.transform.position.y);
         }
+
         if (timeBtwShots <= 0.0f)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKey(KeyCode.Space))
             {
-                Instantiate(projectile, projectileSpawn.position, transform.rotation);
+                Instantiate(projectile, transform.position, transform.rotation);
+                    
                 timeBtwShots = startTimeBtwShots;
             }
         }
