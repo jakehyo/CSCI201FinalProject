1069848f36bc5ce88dfe44c776c0f02b609f2530
jkepstein12 <jkepstei@usc.edu>
2020-04-15 18:25:37 -0700

    Add files via upload

---
+++ Final Project/Final_Project_Client/Final_Project_Client/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Final_Project_Client
+{
+    [Serializable]
+    public class Player
+    {
+        public int playerID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public int highScore { get; set; }
+        public List<int> AllhighScore { get; set; }
+        public bool newGamePlus { get; set; }
+        public List<bool> cosmetic { get; set; }
+        public int weaponID { get; set; }
+        public int money { get; set; }
+    }
+}
