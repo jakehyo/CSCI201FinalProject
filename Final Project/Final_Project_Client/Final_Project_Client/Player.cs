b31908af0e5ff6cadfded253a257a6acc439d74f
jkepstein12 <jkepstei@usc.edu>
2020-04-19 18:34:33 -0700

    Added JSON serialization

--- Final Project/Final_Project_Client/Final_Project_Client/Player.cs
+++ Final Project/Final_Project_Client/Final_Project_Client/Player.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Final_Project_Client
 {
-    [Serializable]
+    [SerializableAttribute]
     public class Player
     {
         public int playerID { get; set; }
         public string username { get; set; }
-        public string password { get; set; }
         public int highScore { get; set; }
         public List<int> AllhighScore { get; set; }
         public bool newGamePlus { get; set; }
@@ -14,4 +15,5 @@ namespace Final_Project_Client
         public int weaponID { get; set; }
         public int money { get; set; }
     }
+
 }
