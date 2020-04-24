4bbecb9b910712ffb25884231bcaa577a2860ee6
16appletond <dappleto@usc.edu>
2020-04-24 15:52:24 -0500

    Added a game over screen

---
+++ RogueLike201/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour
+{
+    public Text gameOver;
+    private float timer = 0.0f;
+    private float fiveSec = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!PlayerScript.alive)
+        {
+            gameOver.enabled = true;
+            timer += Time.deltaTime;
+            
+        }
+        if (timer >= fiveSec)
+        {
+            //use scenechanger to go to main menu
+        }
+    }
+}
