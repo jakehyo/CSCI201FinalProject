55fd1665c0cc52d684048cb2343820eafd07876d
eliashosseiniUSC <ehossein@usc.edu>
2020-04-25 15:43:29 -0700

    looping bg, working on levels

---
+++ RogueLike201/Assets/Scripts/bg_loop.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bg_loop : MonoBehaviour
+{
+    public GameObject obj;
+    private Camera mainCamera;
+    private Vector2 screenBounds;
+    private Vector3 bgPosition;
+    private Vector3 bgVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = gameObject.GetComponent<Camera>();
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        loadChild(obj);
+
+        bgPosition = obj.transform.position;
+        bgVelocity = new Vector3(-0.01f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void loadChild(GameObject obj)
+    {
+        float objWidth = obj.GetComponent<SpriteRenderer>().bounds.size.x;
+        int children = (int)Mathf.Ceil(screenBounds.x * 2 / objWidth);
+        GameObject clone = Instantiate(obj) as GameObject;
+        for (int i = 0; i <= children; i++)
+        {
+            GameObject c = Instantiate(clone) as GameObject;
+            c.transform.SetParent(obj.transform);
+            c.transform.position = new Vector3(objWidth * i, obj.transform.position.y, obj.transform.position.z);
+            c.name = obj.name + i;
+        }
+        Destroy(clone);
+        Destroy(obj.GetComponent<SpriteRenderer>());
+    }
+
+    void repositionChildObjects(GameObject obj)
+    {
+        Transform[] children = obj.GetComponentsInChildren<Transform>();
+        if (children.Length > 1)
+        {
+            GameObject firstChild = children[1].gameObject;
+            GameObject lastChild = children[children.Length-1].gameObject;
+            float halfObjWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
+            if (transform.position.x + screenBounds.x > lastChild.transform.position.x + halfObjWidth)
+            {
+                firstChild.transform.SetAsLastSibling();
+                firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfObjWidth * 2, lastChild.transform.position.y, lastChild.transform.position.z);
+            }
+            else if (transform.position.x - screenBounds.x < firstChild.transform.position.x - halfObjWidth)
+            {
+                lastChild.transform.SetAsFirstSibling();
+                lastChild.transform.position = new Vector3(lastChild.transform.position.x - halfObjWidth * 2, firstChild.transform.position.y, firstChild.transform.position.z);
+            }
+        }
+    }
+
+    void Update()
+    {
+        bgPosition.x += bgVelocity.x;
+        obj.transform.position = bgPosition;
+    }
+
+    void LateUpdate()
+    {
+        repositionChildObjects(obj);
+    }
+}
